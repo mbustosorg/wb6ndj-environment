67d07d0f1d8214231a082f11e26e696db979473d
Mauricio Bustos <mauricio@radiantesg.com>
2023-05-25 17:36:36 +0000

    error handling

--- wb6ndjenvironment/process_messages.py
+++ wb6ndjenvironment/process_messages.py
@@ -2,6 +2,7 @@
 import datetime
 import logging
 import os
+import time
 from dotenv import load_dotenv
 
 import pymysql
@@ -17,33 +18,45 @@ broker_address = os.environ["BROKER_ADDRESS"]
 broker_port = int(os.environ["BROKER_PORT"])
 topics = ["TEMPERATURE_INSIDE", "HUMIDITY_INSIDE", "TEMPERATURE_REPEATER", "HUMIDITY_REPEATER"]
 
+
 sql_engine = create_engine(
     f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}/{os.environ['DB']}",
     pool_recycle=3600,
 )
-db_connection = sql_engine.connect()
 
 
 def on_message(client, userdata, message):
     """Handle a message from the MQTT broker"""
-    df = pd.DataFrame(
-        [[datetime.datetime.utcnow().isoformat(), message.payload.decode()]],
-        columns=["date", message.topic],
-    )
-    df.to_sql(message.topic, db_connection, if_exists="append", index=False)
-    LOGGER.info(
-        f"Message received and saved to file {message.topic}:  {message.payload.decode()}"
-    )
+    try:
+        df = pd.DataFrame(
+            [[datetime.datetime.utcnow().isoformat(), message.payload.decode()]],
+            columns=["date", message.topic],
+        )
+        db_connection = sql_engine.connect()
+        df.to_sql(message.topic, db_connection, if_exists="append", index=False)
+        db_connection.close()
+        LOGGER.info(
+            f"Message received and saved to file {message.topic}:  {message.payload.decode()}"
+        )
+    except:
+        db_connection.rollback()
+        db_connection.close()
 
 
-def run():
-    """Run the MQTT monitor"""
+def mqtt_connect():
+    """Connect and return client"""
     client = mqtt.Client()
     client.on_message = on_message
     client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
     client.username_pw_set(os.environ["MQTT_USERNAME"], os.environ["MQTT_PASSWORD"])
     client.connect(broker_address, broker_port)
+    return client
 
+
+def run():
+    """Run the MQTT monitor"""
+
+    client = mqtt_connect()
     for topic in topics:
         client.subscribe(topic)
 
@@ -51,11 +64,13 @@ def run():
 
     try:
         while True:
-            pass
+            time.sleep(0.5)
 
-    except KeyboardInterrupt:
+    except:
         client.disconnect()
         client.loop_stop()
+
+    run()
 
 
 if __name__ == "__main__":
