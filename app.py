8638df2b063405d7b1430e269089d9f8b3475ee9
Mauricio Bustos <mauricio@radiantesg.com>
2023-06-05 22:22:20 +0000

    html formatting

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 """Flask application to server website for environmental data"""
 import dateutil.parser
+import datetime
 import os
 from zoneinfo import ZoneInfo
 
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine
@@ -28,10 +30,13 @@ def index():
 def data_for_sensor(sensor: str):
     """Get data for 'sensor'"""
     try:
+        start_date = pd.Timestamp.now(tz="UTC") + datetime.timedelta(days=-3)
         db_connection = sql_engine.connect()
         data = pd.read_sql(f"select * from wb6ndjenv.{sensor} order by \"date\"", db_connection)
         db_connection.close()
-        data["date"] = data["date"].apply(lambda x: dateutil.parser.isoparse(x + "Z").astimezone(ZoneInfo("America/Los_Angeles")))
+        data["date"] = data["date"].apply(lambda x: pd.Timestamp(dateutil.parser.isoparse(x + "Z")))
+        data = data[data["date"] >= start_date]
+        data["date"] = data["date"].apply(lambda x: x.astimezone(ZoneInfo("America/Los_Angeles")))
         columns = [data["date"].tolist(), data[sensor].tolist()]
         return jsonify(data=columns)
     except:
